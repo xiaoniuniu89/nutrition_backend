05ce8bdb07bbb9f01b26e49c7993d4adfdf661d4
Daniel Callaghan <dan_cal89@hotmail.com>
2024-01-01 13:15:09 +0000

    Use dynamic port for Heroku

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import app from './src/server';
 import * as dotenv from 'dotenv';
 dotenv.config();
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log('listening on port 3001!');
 });
