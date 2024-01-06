bf24a3a70d868694739a4ec27d9761e6fd4d9de4
Daniel Callaghan <dan_cal89@hotmail.com>
2024-01-06 10:09:01 +0000

    update packages

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,13 @@ const consoleUrl = (
 
 // app.use(consoleUrl);
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
